bce7b7af333e4714c4363986f903325616ee634e
Stefano <viktor.planasul@gmail.com>
2025-06-11 10:19:19 -0300

    Configurações de servidor SMTP
    adicionei as configurações de email

--- src/ia_m_uv/people_counter.py
+++ src/ia_m_uv/people_counter.py
@@ -4,3 +4,9 @@ from datetime import datetime
 from ultralytics import YOLO
 import smtplib
 from email.message import EmailMessage
+
+SMTP = "sandbox.smtp.mailtrap.io"
+PORTA = 587
+EMAIL_ORIGEM = "21e2ea49b6fa16"  # Usuário da conta SMTP
+SENHA_ORIGEM = "c9b69e944702c3"  # Senha do SMTP
+EMAIL_CLIENTE = "cliente@teste.com"  # E-mail de destino (teste)
